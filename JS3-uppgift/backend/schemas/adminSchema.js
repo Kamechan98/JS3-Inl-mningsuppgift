208d132695070f11a07c3e2211ebd156ed092867
Evelina Nyman <ina.nyman98@gmail.com>
2023-05-25 15:50:53 +0200

    commit

--- JS3-uppgift/backend/schemas/adminSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
-
-const adminSchema = new Schema({
-  adminId: { type: Schema.Types.ObjectId, unique: true }
-})
-
-module.exports = mongoose.model('Admin', adminSchema)
--- JS3-uppgift/backend/schemas/orderSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderSchema = mongoose.Schema({
-  customerId: { type: mongoose.Types.ObjectId, ref: 'User', },
-  orderRow: [{ product: { type: mongoose.Types.ObjectId, ref: 'Product' }, quantity: { type: Number } }]
-}, { timestamps: true })
-
-module.exports = mongoose.model('Order', orderSchema)
